f05226633726b96ef0eb7fc0332143558f439eb2
clatonhendricks <250326+clatonhendricks@users.noreply.github.com>
2020-09-23 19:19:31 -0700

    Added files required for Heroku

--- Classify_Birds.py
+++ Classify_Birds.py
@@ -16,18 +16,22 @@ def progress(prog_text):
     with st.spinner(prog_text):
         time.sleep(5)
 
-learn_inf = load_posix_learner(Path()/'export.pkl')
-st.title("Bird classification app")
-st.text("Select an image of a bird")
+def main():
+    st.title("Bird classification app")
+    st.text("Select an image of a bird")
 
-img_path = st.file_uploader("Upload an image",type=['jpg','png'])
+    img_path = st.file_uploader("Upload an image",type=['jpg','png'])
 
 
-if img_path:
-    progress('Detecting. Please wait..')
-    img = P_Image.open(img_path)
-    img_array = np.array(img)
-    st.image(img, use_column_width=True)
-    pred, pred_idx, probs = learn_inf.predict(img_array)
-    prob_pct = probs[pred_idx] * 100
-    st.write('**Prediction**: ', pred, '**Probability:**: ',round(prob_pct.item(),3))
+    if img_path:
+        progress('Detecting. Please wait..')
+        img = P_Image.open(img_path)
+        img_array = np.array(img)
+        st.image(img, use_column_width=True)
+        pred, pred_idx, probs = learn_inf.predict(img_array)
+        prob_pct = probs[pred_idx] * 100
+        st.write('**Prediction**: ', pred, '**Probability:**: ',round(prob_pct.item(),3))
+
+if __name__ == '__main__':
+    learn_inf = load_posix_learner(Path()/'export.pkl')
+    main()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run Classify_Birds.py 
