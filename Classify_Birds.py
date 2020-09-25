0c6914f030da6395e2b0376b6c1cb0c94c4563b3
clatonhendricks <250326+clatonhendricks@users.noreply.github.com>
2020-09-24 17:20:04 -0700

    Added: Banner image, CSS stylesheet etc.

--- Classify_Birds.py
+++ Classify_Birds.py
@@ -3,8 +3,9 @@ from PIL import Image as P_Image
 from fastai.vision.all import *
 import pathlib
 import platform
+import pandas as pd
 
-def load_posix_learner(path):
+def load_path(path):
     save = pathlib.PosixPath
     if platform.system() == 'Windows':
         pathlib.PosixPath = pathlib.WindowsPath
@@ -20,7 +21,13 @@ def progress(prog_text):
     with st.spinner(prog_text):
         time.sleep(5)
 
+def center_img():
+    with open("style.css") as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
 def main():
+    img_banner = P_Image.open('Birds_AutoCollage.jpg')
+    st.image(img_banner, use_column_width=True)
     st.title("Bird classification app")
     st.text("Select an image of a bird")
 
@@ -29,14 +36,19 @@ def main():
 
     if img_path:
         img = P_Image.open(img_path)
-        progress('Detecting. Please wait..')
         img_array = np.array(img)
-        st.image(img, use_column_width=True)
+        st.image(img, width=500)
+        progress('Detecting. Please wait..')
         pred, pred_idx, probs = learn_inf.predict(img_array)
         prob_pct = probs[pred_idx] * 100
+        center_img()
         st.write('**Prediction**: ', pred)
-        st.write('**Probability %:**: ',round(prob_pct.item(),3))
+        st.write('**Probability %:** ',round(prob_pct.item(),3))
+
+
 
 if __name__ == '__main__':
-    learn_inf = load_posix_learner(Path()/'export.pkl')
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    learn_inf = load_path(Path()/'export.pkl')
+    center_img()
     main()
